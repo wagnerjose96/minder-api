614a2e62482e9dd7ca625a7a431924f0c636372a
Wagner José Lima da Silva Junior <wagner.junior@accellog.com>
2018-07-12 19:26:38 -0300

    Criando teste de Usuario

--- src/main/java/br/hela/login/comandos/LogarUsuario.java
+++ src/main/java/br/hela/login/comandos/LogarUsuario.java
@@ -6,4 +6,14 @@ import lombok.Data;
 public class LogarUsuario {
 	private String identificador;
 	private String senha;
+
+	public LogarUsuario() {
+
+	}
+	
+	public LogarUsuario(String identificador, String senha) {
+		this.identificador = identificador;
+		this.senha = senha;
+	}
+
 }
