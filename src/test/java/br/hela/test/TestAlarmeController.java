83f99a2151fb478518f6af290ea1e6b9977d1d92
Wagner José Lima da Silva Junior <wagner.junior@accellog.com>
2018-07-13 16:28:06 -0300

    Criando teste na entidade Alarme

---
+++ src/test/java/br/hela/test/TestAlarmeController.java
@@ -0,0 +1,229 @@
+package br.hela.test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+
+import br.hela.Escoladeti2018Application;
+import br.hela.alarme.Alarme;
+import br.hela.alarme.AlarmeRepository;
+import br.hela.alarme.comandos.CriarAlarme;
+import br.hela.alarme.comandos.EditarAlarme;
+import br.hela.endereco.comandos.CriarEndereco;
+import br.hela.login.LoginController;
+import br.hela.login.comandos.LogarUsuario;
+import br.hela.medicamento.MedicamentoId;
+import br.hela.medicamento.MedicamentoService;
+import br.hela.medicamento.comandos.CriarMedicamento;
+import br.hela.sangue.Sangue;
+import br.hela.sangue.SangueId;
+import br.hela.sangue.SangueRepository;
+import br.hela.sangue.comandos.CriarSangue;
+import br.hela.sexo.Sexo;
+import br.hela.sexo.SexoId;
+import br.hela.sexo.SexoRepository;
+import br.hela.sexo.comandos.CriarSexo;
+import br.hela.telefone.comandos.CriarTelefone;
+import br.hela.usuario.Usuario;
+import br.hela.usuario.UsuarioRepository;
+import br.hela.usuario.UsuarioService;
+import br.hela.usuario.comandos.CriarUsuario;
+
+@RunWith(SpringRunner.class)
+@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class, TransactionDbUnitTestExecutionListener.class })
+@Transactional
+@Rollback
+@WebAppConfiguration
+@SpringBootTest(classes = { Escoladeti2018Application.class }, webEnvironment = WebEnvironment.MOCK)
+
+public class TestAlarmeController {
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private SangueRepository repoSangue;
+
+	@Autowired
+	private SexoRepository repoSexo;
+
+	@Autowired
+	private LoginController login;
+
+	@Autowired
+	private UsuarioService serviceUsuario;
+
+	@Autowired
+	private MedicamentoService serviceMedicamento;
+
+	@Autowired
+	private UsuarioRepository repo;
+
+	@Autowired
+	private AlarmeRepository repoAlarme;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+
+	@Test
+	public void testCadastrar() throws Exception {
+		SangueId idSangue = criarSangue("A+");
+		SexoId idSexo = criarSexo("Masculino");
+
+		serviceUsuario.salvar(criarUsuario("wagner@hotmail.com", "wagnerju", idSexo, idSangue)).get();
+		MedicamentoId idMedicamento = serviceMedicamento.salvar(criarMedicamento("DorFlex", "100mg")).get();
+
+		List<Usuario> usuarios = repo.findAll();
+		assertThat(usuarios.get(0), notNullValue());
+
+		final String jsonString = objectMapper.writeValueAsString(criarAlarme(idMedicamento));
+
+		this.mockMvc
+				.perform(post("/alarmes").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+						.header("token", logar("wagnerju", "1234")).content(jsonString))
+				.andExpect(jsonPath("$", equalTo("O alarme foi cadastrado com sucesso")))
+				.andExpect(status().isCreated());
+	}
+
+	@Test
+	public void testEditar() throws Exception {
+		SangueId idSangue = criarSangue("A+");
+		SexoId idSexo = criarSexo("Masculino");
+
+		serviceUsuario.salvar(criarUsuario("wagner@hotmail.com", "wagnerju", idSexo, idSangue)).get();
+		MedicamentoId idMedicamento = serviceMedicamento.salvar(criarMedicamento("DorFlex", "100mg")).get();
+
+		List<Usuario> usuarios = repo.findAll();
+		assertThat(usuarios.get(0), notNullValue());
+
+		String jsonString = objectMapper.writeValueAsString(criarAlarme(idMedicamento));
+
+		this.mockMvc
+				.perform(post("/alarmes").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+						.header("token", logar("wagnerju", "1234")).content(jsonString))
+				.andExpect(jsonPath("$", equalTo("O alarme foi cadastrado com sucesso")))
+				.andExpect(status().isCreated());
+
+		List<Alarme> alarmes = repoAlarme.findAll();
+		assertThat(alarmes.get(0), notNullValue());
+
+		jsonString = objectMapper.writeValueAsString(editarAlarme(alarmes.get(0)));
+		System.out.println(jsonString);
+
+		this.mockMvc
+				.perform(put("/alarmes").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+						.header("token", logar("wagnerju", "1234")).content(jsonString))
+				.andExpect(jsonPath("$", equalTo("O alarme foi alterado com sucesso"))).andExpect(status().isOk());
+
+	}
+
+	private CriarAlarme criarAlarme(MedicamentoId idMedicamento) {
+		CriarAlarme alarme = new CriarAlarme();
+		alarme.setDataInicio(Date.valueOf(LocalDate.of(2018, 07, 10)));
+		alarme.setDataFim(Date.valueOf(LocalDate.of(2018, 07, 20)));
+		alarme.setDescricao("Tomar medicamento");
+		alarme.setIdMedicamento(idMedicamento);
+		alarme.setPeriodicidade(8);
+		alarme.setQuantidade("1");
+		return alarme;
+	}
+
+	private CriarMedicamento criarMedicamento(String nome, String composicao) {
+		CriarMedicamento medicamento = new CriarMedicamento();
+		medicamento.setComposicao(composicao);
+		medicamento.setNomeMedicamento(nome);
+		return medicamento;
+	}
+
+	private CriarUsuario criarUsuario(String email, String username, SexoId idSexo, SangueId idSangue) {
+		CriarEndereco endereco = new CriarEndereco();
+		endereco.setBairro("Zona 6");
+		endereco.setCidade("Maringá");
+		endereco.setEstado("Paraná");
+		endereco.setNumero(1390);
+		endereco.setRua("Castro Alves");
+
+		CriarTelefone telefone = new CriarTelefone();
+		telefone.setDdd(44);
+		telefone.setNumero(999038860);
+
+		CriarUsuario usuario = new CriarUsuario();
+		usuario.setNome("Wagner José");
+		usuario.setEmail(email);
+		usuario.setDataNascimento(Date.valueOf(LocalDate.of(1997, 03, 17)));
+		usuario.setEndereco(endereco);
+		usuario.setIdSangue(idSangue);
+		usuario.setIdSexo(idSexo);
+		usuario.setSenha("1234");
+		usuario.setTelefone(telefone);
+		usuario.setUsername(username);
+
+		return usuario;
+	}
+
+	private SangueId criarSangue(String tipo) {
+		SangueId id = repoSangue.save(new Sangue(new CriarSangue(tipo))).getIdSangue();
+		return id;
+	}
+
+	private SexoId criarSexo(String tipo) {
+		SexoId id = repoSexo.save(new Sexo(new CriarSexo(tipo))).getIdGenero();
+		return id;
+	}
+
+	private String logar(String nomeUsuario, String senha) {
+		LogarUsuario corpoLogin = new LogarUsuario();
+		corpoLogin.setIdentificador(nomeUsuario);
+		corpoLogin.setSenha(senha);
+		return login.loginUsuario(corpoLogin).getBody();
+	}
+
+	private EditarAlarme editarAlarme(Alarme alarme) {
+		EditarAlarme alarmeEditado = new EditarAlarme();
+		alarmeEditado.setId(alarme.getId());
+		alarmeEditado.setDataInicio(Date.valueOf(LocalDate.of(2018, 07, 10)));
+		alarmeEditado.setDataFim(Date.valueOf(LocalDate.of(2018, 07, 20)));
+		alarmeEditado.setDescricao("Tomar medicamento Editado !!!");
+		alarmeEditado.setIdMedicamento(alarme.getIdMedicamento());
+		alarmeEditado.setPeriodicidade(8);
+		alarmeEditado.setQuantidade("1");
+		return alarmeEditado;
+	}
+
+}
